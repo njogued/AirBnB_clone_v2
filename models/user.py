842d0e92ca2d0d6ca199213e6b3c47d48464831b
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-22 19:28:36 +0000

    user schema updated

--- models/user.py
+++ models/user.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel
-from models.base_model import Base
+from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
@@ -13,5 +12,3 @@ class User(BaseModel, Base):
     password = Column(String(128), nullable=False)
     first_name = Column(String(128), nullable=False)
     last_name = Column(String(128), nullable=False)
-
-    places = relationship("Place", backref="user", cascade="all, delete")
