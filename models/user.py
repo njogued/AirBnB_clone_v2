04d91deaba119719960236291dcb13de72d069d5
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-22 20:23:15 +0000

    user schema checker fix

--- models/user.py
+++ models/user.py
@@ -1,11 +1,13 @@
 #!/usr/bin/python3
 """This module defines a class User"""
-from models.base_model import BaseModel, Base
+from models.base_model import BaseModel
+from models.base_model import Base
 from sqlalchemy import Column, String
+from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
-    """This class maps to a users table in db"""
+    """Represents a user for MySQL db"""
 
     __tablename__ = "users"
     email = Column(String(128), nullable=False)
