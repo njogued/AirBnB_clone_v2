6fca060b9447b325abc2bc678d8d94858d6135dc
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-22 20:53:31 +0000

    update place table

--- models/place.py
+++ models/place.py
@@ -3,6 +3,7 @@
 from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column, String, Integer, Float, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class Place(BaseModel, Base):
