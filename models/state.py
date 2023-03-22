5358e5662fad4ee4f7bf5b0f7b05f95f06f721f5
njogued <njogued@gmail.com>
2023-03-22 11:25:51 -0700

    modes/state updated

--- models/state.py
+++ models/state.py
@@ -11,5 +11,10 @@ class State(BaseModel, Base):
     """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-
     cities = relationship("City", backref="state", cascade="all, delete")
+
+    if ge("HBNB_TYPE_STORAGE") != 'db':
+        @property
+        def cities(self):
+            '''return list of cities if not using a database'''
+            return [city for city in self.cities]
