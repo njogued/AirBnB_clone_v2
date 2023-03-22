3b03f53e5c9325e2d735f079c2b025eec7327bf7
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-22 20:45:22 +0000

    update user & place slasses

--- models/place.py
+++ models/place.py
@@ -20,3 +20,5 @@ class Place(BaseModel, Base):
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
     amenity_ids = []
+
+    reviews = relationship("Review", backref="place", cascade="delete")
