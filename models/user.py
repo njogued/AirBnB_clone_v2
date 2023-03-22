5d5f8fe4e05cf0221e87ba329253e046a68b89d5
Gitau Kiarie <igitaukiarie@gmail.com>
2023-03-22 20:20:08 +0000

    user table schema fix

--- models/user.py
+++ models/user.py
@@ -2,13 +2,12 @@
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
 
 
 class User(BaseModel, Base):
     """This class maps to a users table in db"""
 
-    __tablename__ = 'users'
+    __tablename__ = "users"
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
     first_name = Column(String(128))
