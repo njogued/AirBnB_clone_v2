649d1fdd7faa5396a075888207129c7a91314d16
Gitau Kiarie <igitaukiarie@gmail.com>
2023-04-06 20:08:46 +0000

    task 2 comment updated

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -13,25 +13,48 @@ env.hosts = ['54.175.228.113', '54.236.26.145']
 env.user = 'ubuntu'
 env.key_filename = '~/.ssh/school'
 
+
 def do_deploy(archive_path):
     """ftn deploys code and decompresses it"""
-    
     if not os.path.isfile(archive_path):
         return False
+
+    """extract filename of archive from its path"""
     comp_file = archive_path.split("/")[-1]
+
+    """removing the file extension .tgz"""
     no_suffix = comp_file.split(".")[0]
-    
+
     try:
-       remote_dir = "/data/web_static/releases/{}/".format(no_suffix)
-       symbolic_link = "/data/web_static/current"
-       put(archive_path, "/tmp/")
-       run("sudo mkdir -p {}".format(remote_dir))
-       run("sudo tar -xvzf /tmp/{} -C {}".format(comp_file, remote_dir))
-       run("sudo rm /tmp/{}".format(comp_file))
-       run("sudo mv {}/web_static/* {}".format(remote_dir, remote_dir))
-       run("sudo rm -rf {}/web_static".format(remote_dir))
-       run("sudo rm -rf /data/web_static/current")
-       run("sudo ln -sf {} {}".format(remote_dir, symbolic_link))
-       return True
+        """remote dir where the code will be deployed"""
+        remote_dir = "/data/web_static/releases/{}/".format(no_suffix)
+
+        """symbolic link; will point to latest version of the code"""
+        symbolic_link = "/data/web_static/current"
+
+        """uploading archive to /tmp/ in server"""
+        put(archive_path, "/tmp/")
+
+        """create remote directory if not exists"""
+        run("sudo mkdir -p {}".format(remote_dir))
+
+        """extracts contents of archive to remote dir"""
+        run("sudo tar -xvzf /tmp/{} -C {}".format(comp_file, remote_dir))
+
+        """delete archive from server"""
+        run("sudo rm /tmp/{}".format(comp_file))
+
+        """move contents of webstatic to remote dir"""
+        run("sudo mv {}/web_static/* {}".format(remote_dir, remote_dir))
+
+        """delete the webstatic dir from remote dir"""
+        run("sudo rm -rf {}/web_static".format(remote_dir))
+
+        """removes symbolic link to previous code version"""
+        run("sudo rm -rf /data/web_static/current")
+
+        """create new symbolic link to new code version"""
+        run("sudo ln -sf {} {}".format(remote_dir, symbolic_link))
+        return True
     except Exception as e:
-       return False
+        return False
