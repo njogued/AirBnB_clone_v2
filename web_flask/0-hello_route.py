0d55bbb9e436d6f53e65b1939e18cc90c9570d51
njogued <njogued@gmail.com>
2023-04-20 05:27:20 -0700

    Web flask task 1

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,11 +1,14 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 """First flask app"""
 from flask import Flask
 
 hbnb = Flask(__name__)
+
+
 @hbnb.route("/", strict_slashes=False)
 def index():
-	return "Hello HBNB"
+    return "Hello HBNB"
+
 
 if __name__ == "__main__":
-	hbnb.run(host="0.0.0.0", port=5000)
+    hbnb.run(host="0.0.0.0", port=5000)
---
+++ web_flask/1-hbnb_route.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Flask task 1"""
+from flask import Flask
+
+
+alxhbnb = Flask(__name__)
+
+
+@alxhbnb.route("/", strict_slashes=False)
+def hbnb():
+    return "Hello HBNB!"
+
+
+@alxhbnb.route("/hbnb", strict_slashes=False)
+def hbnb_display():
+    return "HBNB"
+
+
+if __name__ == "__main__":
+    alxhbnb.run(host="0.0.0.0", port=5000)
